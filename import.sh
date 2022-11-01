c7d778f60e077980b59a9a0831f1c12bb0ebcca9
s2 <stubrown357@gmail.com>
2022-10-31 22:43:06 -0500

    notes in comments

--- import.sh
+++ import.sh
@@ -1,3 +1,12 @@
 source config.cfg
 
 ls $import_filepath
+ls $target_filepath
+
+# array of unique dates found in file
+# for each day in array_dates
+#   mkdir day
+#   mv files with creation/modified date
+#   mkdir raw and mkdir jpg
+#   mv '*.nef' to raw folder
+#   mv '*.jpg' to raw folder
