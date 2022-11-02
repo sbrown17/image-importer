5d0e236c3306db26446138ca802bb0f5cd6fd8c9
s2 <stubrown357@gmail.com>
2022-11-01 22:36:39 -0500

    Work in progress: loop through raw image files

--- import.sh
+++ import.sh
@@ -1,9 +1,19 @@
 source config.cfg
 
-ls $import_filepath
-ls $target_filepath
+# ls $import_filepath
+# ls $target_filepath
 
 # array of unique dates found in file
+
+
+for file in $import_filepath
+do
+    if [ ${file: -4} == ".nef" ]; then # check if last 4 chars of filename has .nef (is a raw image)
+        # do stuff
+    fi
+done
+
+
 # for each day in array_dates
 #   mkdir day
 #   mv files with creation/modified date
